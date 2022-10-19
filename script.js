26b1e328f2e4cdd6171e67b623695c96571c40c7
osmaryupanqui <osmar30asis@gmail.com>
2022-10-18 23:02:35 -0500

    modified js

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const buttons = document.querySelectorAll('.btn')
+const mathButtons = document.querySelectorAll('.btn-math')
+const divBtn = document.querySelector('#divBtn')
+const multBtn = document.querySelector('#multBtn')
+const subtBtn = document.querySelector('#subtBtn')
+const pointBtn = document.querySelector('#pointBtn')
+const equalBtn = document.querySelector('#equalBtn')
+const sumBtn = document.querySelector('#sumBtn')
+
 const screenUp = document.querySelector('.upper-line')
 const screenDown = document.querySelector('.down-line')
 
+screenDown.textContent = 0
+
 buttons.forEach(btn => {
     btn.addEventListener('click', (e => {
-        console.log(e)
+        if (screenDown.textContent === '0') {
+            screenDown.textContent = btn.textContent
+        } else screenDown.textContent += btn.textContent
+    }))
+})
+/*
+divBtn.addEventListener('click', (e => {
+    let val1 = Number(screenDown.textContent)
+    screenUp.textContent = screenDown.textContent + ' ' + divBtn.textContent
+    screenDown.textContent = ''
+}))
+*/
+
+mathButtons.forEach(btn => {
+    btn.addEventListener('click', (e => {
+        let val1 = Number(screenDown.textContent)
+        screenDown.textContent += btn.textContent
     }))
 })
