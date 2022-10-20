aabd3e6927d68820707974dc4864edd2af41deb2
osmaryupanqui <osmar30asis@gmail.com>
2022-10-19 20:47:03 -0500

    finished the js and added a reset button

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const buttons = document.querySelectorAll('.btn')
-const mathButtons = document.querySelectorAll('.btn-math')
-const divBtn = document.querySelector('#divBtn')
-const multBtn = document.querySelector('#multBtn')
-const subtBtn = document.querySelector('#subtBtn')
-const pointBtn = document.querySelector('#pointBtn')
-const equalBtn = document.querySelector('#equalBtn')
-const sumBtn = document.querySelector('#sumBtn')
+const buttons = document.querySelectorAll('.btn');
+const mathButtons = document.querySelectorAll('.btn-math');
 
-const screenUp = document.querySelector('.upper-line')
-const screenDown = document.querySelector('.down-line')
+const equalBtn = document.querySelector('#equalBtn');
 
-screenDown.textContent = 0
+const screenUp = document.querySelector('.upper-line');
+const screenDown = document.querySelector('.down-line');
+
+const resetBtn = document.querySelector('.reseter');
+
+screenUp.textContent = '';
+screenDown.textContent = 0;
+
+let val1 = 0;
+let val2 = 0;
+let operator = '';
+
+function addition (val1, val2) {
+    return val1 + val2;
+}
+
+function subtract (val1, val2) {
+    return val1 - val2;
+}
+
+function multiplication (val1, val2) {
+    return val1 * val2;
+}
+
+function division (val1, val2) {
+    if (val2 === 0) {
+        alert('You broke the calculator!')
+        return 'null'}
+    else return val1 / val2;
+}
 
 buttons.forEach(btn => {
     btn.addEventListener('click', (e => {
@@ -19,17 +41,39 @@ buttons.forEach(btn => {
         } else screenDown.textContent += btn.textContent
     }))
 })
-/*
-divBtn.addEventListener('click', (e => {
-    let val1 = Number(screenDown.textContent)
-    screenUp.textContent = screenDown.textContent + ' ' + divBtn.textContent
-    screenDown.textContent = ''
-}))
-*/
 
 mathButtons.forEach(btn => {
     btn.addEventListener('click', (e => {
-        let val1 = Number(screenDown.textContent)
-        screenDown.textContent += btn.textContent
+        val1 = Number(screenDown.textContent);
+        screenUp.textContent = screenDown.textContent;
+        screenUp.textContent += btn.textContent;
+        screenDown.textContent = '0'
+        operator = btn.textContent
     }))
 })
+
+equalBtn.addEventListener('click', (e => {
+    val2 = Number(screenDown.textContent);
+    screenUp.textContent += screenDown.textContent
+
+    switch (operator) {
+        case '+':
+            return screenDown.textContent = addition (val1, val2);
+        case '-':
+            return screenDown.textContent = subtract (val1, val2);
+        case '*':
+            return screenDown.textContent = multiplication (val1, val2);
+        case '/':
+            return screenDown.textContent = division (val1, val2);
+        default:
+            return null
+    }
+}))
+
+resetBtn.addEventListener('click', (e => {
+    let val1 = 0;
+    let val2 = 0;
+    let operator = '';
+    screenDown.textContent = '0'
+    screenUp.textContent = ''
+}))
